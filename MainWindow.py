deac60d09e7a92488cfdf889c0cf4bc71090ca8b
Anna4142 <103633351+Anna4142@users.noreply.github.com>
2023-12-05 11:40:25 +0200

    handling 2 different simulated mice changes
    # Conflicts:
    #	ExperimentManager.py

--- MainWindow.py
+++ MainWindow.py
@@ -14,16 +14,25 @@ def main():
             num_trials = settings.get('num_trials')
             trial_duration = settings.get('trial_duration')
             decision_time = settings.get('decision_time')
-            opponent_strategy = settings.get('opponent_strategy')
-            opponent_type = settings.get('opponent_type')  # Make sure 'opponent_type' is included in the settings
 
+            opponent_type = settings.get('opponent_type')  # Make sure 'opponent_type' is included in the settings
+            if opponent_type=="mouse_computer":
+                opponent_strategy_1 = settings.get('opponent_strategy')
+                opponent_strategy_2= settings.get('computer_opponent_strategy')
+            elif  opponent_type =="computer_computer":
+                opponent_strategy_1 = settings.get('opponent_strategy')
+                opponent_strategy_2 = settings.get('computer_opponent_strategy')
+            else:
+
+                opponent_strategy_1 = settings.get('opponent_strategy')
+                opponent_strategy_2 = settings.get('computer_opponent_strategy')
             # Modify num_trials as per your requirement
             num_trials += 2
 
             # Initialize and start the experiment
             expManager = ExperimentManager()
             print("Experiment manager now running")
-            expManager.start_streaming_exp(num_trials, trial_duration, decision_time, opponent_type, opponent_strategy)
+            expManager.start_streaming_exp(num_trials, trial_duration, decision_time, opponent_type, opponent_strategy_1,opponent_strategy_2)
             del expManager
         else:
             print("No valid settings were provided.")
